20227e10b85fef37be0b43c18d5d768700f40c43
Adle24 <askaradilet41@gmail.com>
2025-09-23 19:22:25 +0500

    setted up simple server

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def main():
-    print("Hello from bookly!")
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+class BookCreateModel(BaseModel):
+    title: str
+    author: str
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+
+@app.get("/greet")
+async def greet_name(age: int, name: str | None = "User") -> dict[str, str]:
+    return {"message": f"Hello, {name}. You are {age} years old."}
+
+
+@app.post("/create_book")
+async def create_book(book_data: BookCreateModel):
+    return {
+        "title": book_data.title,
+        "author": book_data.author,
+    }
+
+
+@app.get("/get_headers")
+async def get_headers(
+    accept: str = Header(None),
+    content_type: str = Header(None),
+    user_agent: str = Header(None),
+    host: str = Header(None),
+):
+    request_headers = {
+        "Accept": accept,
+        "Content-Type": content_type,
+        "User-Agent": user_agent,
+        "Host": host,
+    }
+
+    return request_headers
