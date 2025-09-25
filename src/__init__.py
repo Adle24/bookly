ae79cd9b8574dbd85cedd751821cbd1ab02b6025
Adle24 <askaradilet41@gmail.com>
2025-09-25 14:44:22 +0500

    created books table

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from src.books.routes import book_router
+from src.db.main import init_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
 
 version = "v1"
 app = FastAPI(
     version=version,
     title="Bookly",
     description="Bookly REST API for book review web service",
+    lifespan=lifespan,
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=["books"])
