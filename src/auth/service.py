bb93da161a184808b726ad6306a5377da6f2560f
Adle24 <askaradilet41@gmail.com>
2025-09-27 18:31:55 +0500

    added jwt handling functions

--- src/auth/service.py
+++ src/auth/service.py
@@ -21,7 +21,7 @@ class UserService:
     async def create_user(self, user_data: UserCreateModel, session: AsyncSession):
         user_data_dict = user_data.model_dump()
         new_user = User(**user_data_dict)
-        new_user.password_hash  = generate_password_hash(user_data_dict["password"])
+        new_user.password_hash = generate_password_hash(user_data_dict["password"])
         session.add(new_user)
         await session.commit()
         return new_user
--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,5 +1,13 @@
+import logging
+import uuid
+from datetime import datetime, timedelta
+
+import jwt
 from passlib.context import CryptContext
 
+from src.config import Config
+
+ACCESS_TOKEN_EXPIRY = 3600
 password_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
@@ -9,3 +17,33 @@ def generate_password_hash(password: str) -> str:
 
 def verify_password_hash(password: str, hashed_password: str) -> bool:
     return password_context.verify(password, hashed_password)
+
+
+def create_access_token(
+    user_data: dict, expiry: timedelta = None, refresh: bool = False
+):
+    payload = {
+        "user": user_data,
+        "exp": datetime.now() + expiry
+        if expiry is not None
+        else timedelta(seconds=ACCESS_TOKEN_EXPIRY),
+        "jti": str(uuid.uuid4()),
+        "refresh": refresh,
+    }
+
+    token = jwt.encode(
+        payload=payload, key=Config.JWT_SECRET, algorithm=Config.JWT_ALGORITHM
+    )
+    return token
+
+
+def decode_token(token: str):
+    try:
+        token_data = jwt.decode(
+            jwt=token, key=Config.JWT_SECRET, algorithms=[Config.JWT_ALGORITHM]
+        )
+
+        return token_data
+    except jwt.PyJWTError as e:
+        logging.exception(e)
+        return None
