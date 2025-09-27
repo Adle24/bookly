b4a8471b94e488f87c9e8252834e8cf7924741e2
Adle24 <askaradilet41@gmail.com>
2025-09-27 17:46:34 +0500

    added user creation api

---
+++ src/auth/schemas.py
@@ -0,0 +1,25 @@
+import uuid
+from datetime import datetime
+
+from pydantic import BaseModel
+from sqlmodel import Field
+
+
+class UserCreateModel(BaseModel):
+    firstname: str = Field(max_length=25)
+    lastname: str = Field(max_length=25)
+    username: str = Field(max_length=8)
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)
+
+
+class UserModel(BaseModel):
+    uid: uuid.UUID
+    username: str
+    email: str
+    firstname: str
+    lastname: str
+    password_hash: str = Field(exclude=True)
+    is_verified: bool
+    created_at: datetime
+    updated_at: datetime
